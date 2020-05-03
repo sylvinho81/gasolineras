f240ff8d7c48b049086044eba846dc21afdfc21f
ptorre@grayhairsoftware.com <ptorre@grayhairsoftware.com>
2020-05-03 14:23:21 +0200

    Footer.

--- src/configuration.js
+++ src/configuration.js
@@ -1,8 +1,8 @@
 export const Config = {
-  apiIndexUrl: 'https://gasolineras-spain-api.herokuapp.com/gas_stations',
-  apiSearchUrl: 'https://gasolineras-spain-api.herokuapp.com/gas_stations/search',
-  apiAutocompleteUrl: 'https://gasolineras-spain-api.herokuapp.com/gas_stations/autocomplete',
-  apiKey: "AIzaSyBqwOPZmXqQK-FTD-TZWPuDF_NfJErJbOk",
+  apiIndexUrl: '/gas_stations',
+  apiSearchUrl: '/gas_stations/search',
+  apiAutocompleteUrl: '/gas_stations/autocomplete',
+  apiKey: "",
   latMadrid: 40.416729,
   longMadrid: -3.703339
 
