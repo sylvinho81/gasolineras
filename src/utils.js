d6a3d8be83ddb8d410a4dbab71d5624e053db36c
ptorre@grayhairsoftware.com <ptorre@grayhairsoftware.com>
2020-05-10 14:31:19 +0200

    utc

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,8 @@
 var formatDate = function(string) {
   var options = { year: 'numeric', month: 'numeric', day: 'numeric', hour: '2-digit',
-    minute:'2-digit', timeZone: "Europe/Madrid" };
+    minute:'2-digit', timeZone: "UTC-2" };
   var spainTime = new Date(string).toLocaleString('es-ES',options);
-  var d = new Date(spainTime);
-  return d.toLocaleDateString() + ' ' + d.toTimeString().substring(0, d.toTimeString().indexOf("GMT"));
+  return spainTime;
 }
 
 exports.formatDate = formatDate;
