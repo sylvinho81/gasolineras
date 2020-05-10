f07f2286653b19655d12b65b6b87b41048d35438
ptorre@grayhairsoftware.com <ptorre@grayhairsoftware.com>
2020-05-10 14:14:06 +0200

    timezone

--- src/utils.js
+++ src/utils.js
@@ -2,7 +2,7 @@ var formatDate = function(string) {
   var options = { year: 'numeric', month: 'numeric', day: 'numeric', hour: '2-digit',
     minute:'2-digit', timeZone: "Europe/Madrid" };
   var spainTime = new Date(string).toLocaleString('es-ES',options);
-  return (new Date(spainTime)).toISOString();
+  return spainTime;
 }
 
 exports.formatDate = formatDate;
