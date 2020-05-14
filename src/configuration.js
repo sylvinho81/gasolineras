05ca1a97344affd38e80f12869deaa1479ce8ed5
Pablo Torre <pablotorre81@gmail.com>
2020-05-14 14:28:17 +0200

    gas stations by province

--- src/configuration.js
+++ src/configuration.js
@@ -3,7 +3,7 @@ export const Config = {
   apiSearchUrl: 'https://gasolineras-spain-api.herokuapp.com/gas_stations/search',
   apiAutocompleteUrl: 'https://gasolineras-spain-api.herokuapp.com/gas_stations/autocomplete',
   apiDetailUrl: 'https://gasolineras-spain-api.herokuapp.com/gas_stations/',
-  apiProvinceUrl: 'https://gasolineras-spain-api.herokuapp.com/by_province/',
+  apiProvinceUrl: 'https://gasolineras-spain-api.herokuapp.com/gas_stations/by_province/',
   apiKey: "AIzaSyBqwOPZmXqQK-FTD-TZWPuDF_NfJErJbOk",
   apiKeyGeo: "5be693f77001bbd7b1dd9035ca7737f6",
   latMadrid: 40.416729,
