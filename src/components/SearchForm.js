243f4dfe17a177b3dfd4e9e7d17b018e6a2c47ff
Pablo Torre <pablotorre81@gmail.com>
2020-05-16 14:49:48 +0200

    Fix #1 #3

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -3,8 +3,6 @@ import {Config} from '../configuration'
 import Autocomplete from './AutocompleteInput'
 import Radio from '@material-ui/core/Radio'
 import FormControlLabel from '@material-ui/core/FormControlLabel';
-import pin_normal from '../assets/pin_normal.png'
-import pin_cheap from '../assets/pin_cheap.png'
 const URL_API_SEARCH = Config.apiSearchUrl
 const SELECTED_RADIO_BY_DEFAULT = Config.selectedRadioByDefault
 
@@ -78,8 +76,6 @@ export class SearchForm extends Component {
               <FormControlLabel value="price_gasoline_98" control={<Radio />} label="Gasolina 98" onChange={this._handleChange} checked={selectedRadio === 'price_gasoline_98'}/>
               <FormControlLabel value="price_diesel_a" control={<Radio />} label="Diesel A" onChange={this._handleChange} checked={selectedRadio === 'price_diesel_a'}/>
               <FormControlLabel value="price_diesel_b" control={<Radio />} label="Diesel B " onChange={this._handleChange} checked={selectedRadio === 'price_diesel_b'}/>
-              <img src={pin_cheap} alt={"Mejor precio"} height={"40px"}/><span style={{marginLeft: "2px", marginRight: "2px"}}>Mejor precio</span>
-              <img src={pin_normal} alt={"Precio normal"} height={"40px"}/><span style={{marginLeft: "3px", marginRight: "2px"}}>Precio normal</span>
             </div>
           </div>
         </div>
