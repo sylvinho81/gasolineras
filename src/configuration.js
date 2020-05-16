9cd01306e854487eed3f1ee8696e3538481821a8
Pablo Torre <pablotorre81@gmail.com>
2020-05-16 14:51:04 +0200

    Deployment filters by prices

--- src/configuration.js
+++ src/configuration.js
@@ -1,11 +1,11 @@
 export const Config = {
-  apiIndexUrl: '/gas_stations',
-  apiSearchUrl: '/gas_stations/search',
-  apiAutocompleteUrl: '/gas_stations/autocomplete',
-  apiDetailUrl: '/gas_stations/',
-  apiProvinceUrl: '/gas_stations/by_province/',
-  apiKey: "",
-  apiKeyGeo: "",
+  apiIndexUrl: 'https://gasolineras-spain-api.herokuapp.com/gas_stations',
+  apiSearchUrl: 'https://gasolineras-spain-api.herokuapp.com/gas_stations/search',
+  apiAutocompleteUrl: 'https://gasolineras-spain-api.herokuapp.com/gas_stations/autocomplete',
+  apiDetailUrl: 'https://gasolineras-spain-api.herokuapp.com/gas_stations/',
+  apiProvinceUrl: 'https://gasolineras-spain-api.herokuapp.com/gas_stations/by_province/',
+  apiKey: "AIzaSyBqwOPZmXqQK-FTD-TZWPuDF_NfJErJbOk",
+  apiKeyGeo: "5be693f77001bbd7b1dd9035ca7737f6",
   latMadrid: 40.416729,
   longMadrid: -3.703339,
   selectedRadioByDefault: 'all'
